1b9ea4dfdffc7be69cb5df170058fe126ae22b3b
JesusCamiloGamezDiaz <jesus.camilo.gamez.diaz.1989@gmail.com>
2023-10-08 07:29:29 -0600

    Adicionando el insertar

--- WebApiRepos/Controllers/WeatherForecastController.cs
+++ WebApiRepos/Controllers/WeatherForecastController.cs
@@ -29,5 +29,18 @@ namespace WebApiRepos.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> ObtenerWeatherForecast(int TemperatureF)
+        {
+            var lista = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            });
+
+            return lista.Where(t => t.TemperatureF == TemperatureF).ToArray();
+        }
     }
 }
